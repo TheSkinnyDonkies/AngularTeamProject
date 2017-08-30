4a4bf9ddd07de43a73c080053f89d37afbbf4b9b
shopov91 <shopov91@gmail.com>
2017-08-30 21:07:35 +0300

    added and enabled bootstrap

--- TeamApp/src/app/app.module.ts
+++ TeamApp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AlertModule } from 'ngx-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -12,7 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
