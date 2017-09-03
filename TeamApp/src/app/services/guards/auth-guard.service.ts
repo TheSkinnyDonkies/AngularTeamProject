fdb292ba2246f3a4b35c474bb77937f7d77a5e2f
shopov91 <shopov91@gmail.com>
2017-09-03 09:45:49 +0300

    auth-guard.service.ts implemented

--- TeamApp/src/app/services/guards/auth-guard.service.ts
+++ TeamApp/src/app/services/guards/auth-guard.service.ts
@@ -2,16 +2,26 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/take';
+import * as firebase from 'firebase/app';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import * as firebase from 'firebase/app';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
-        return true;
+
+    user: Observable<firebase.User>;
+
+    constructor(private afAuth: AngularFireAuth, private router: Router) {
+        this.user = afAuth.authState;
     }
 
-    constructor() { }
-
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        return this.user.map((auth) => {
+            if (!auth) {
+                this.router.navigateByUrl('/login');
+                return false;
+            }
+            return true;
+        }).take(1);
+    }
 }
