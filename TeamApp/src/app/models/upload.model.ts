88aaab31159b5c78b3ef66fd1ea6055aad1ea6cf
shopov91 <shopov91@gmail.com>
2017-09-06 18:13:12 +0300

    gallery images display when and who uploaded them

--- TeamApp/src/app/models/upload.model.ts
+++ TeamApp/src/app/models/upload.model.ts
@@ -1,12 +1,24 @@
+import { AngularFireAuth } from 'angularfire2/auth';
+import { User } from './user.model';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+
 export class Upload {
     $key: string;
     name: string;
     file: File;
     url: string;
     progress: number;
-    createdOn: any = new Date();
+    uploadedOn: any;
+    uploadedBy: any;
 
-    constructor(currentFile: File) {
-        this.file = currentFile ;
+    constructor(private currentFile: File, afAuth: AngularFireAuth) {
+        this.file = currentFile;
+        afAuth.authState.subscribe(auth => {
+            if (auth !== undefined && auth !== null) {
+                this.uploadedBy = auth.email;
+            }
+        });
+        this.uploadedOn = (new Date()).toString();
     }
 }
--- TeamApp/src/app/modules/gallery/upload/upload.component.ts
+++ TeamApp/src/app/modules/gallery/upload/upload.component.ts
@@ -1,3 +1,4 @@
+import { AngularFireAuth } from 'angularfire2/auth';
 import { Upload } from './../../../models/upload.model';
 import { Component, OnInit } from '@angular/core';
 import { UploadService } from './../../../services/upload.service';
@@ -13,14 +14,14 @@ export class UploadComponent implements OnInit {
   files: FileList;
   upload: Upload;
   isSendingFilesEnabled: boolean;
-  constructor(private uploadService: UploadService) { }
+  constructor(private uploadService: UploadService, private afAuth: AngularFireAuth) { }
 
   uploadFiles() {
     const filesToUpload = this.files;
     const filesIndexs = _.range(filesToUpload.length);
     _.each(filesIndexs, (index) => {
       console.log(filesToUpload[index]);
-      this.upload = new Upload(filesToUpload[index]);
+      this.upload = new Upload(filesToUpload[index], this.afAuth);
       this.uploadService.uploadFile(this.upload);
       this.isSendingFilesEnabled = !this.isSendingFilesEnabled;
     });
