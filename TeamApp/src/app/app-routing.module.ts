ebbd2934d29dd6eacc9adae938ce2aeec62ef0b2
shopov91 <shopov91@gmail.com>
2017-09-07 14:24:44 +0300

    user and gallery modules are now lazy loaded

--- TeamApp/src/app/app-routing.module.ts
+++ TeamApp/src/app/app-routing.module.ts
@@ -1,14 +1,4 @@
-import { UserComponent } from './modules/user-administration/user/user.component';
-import { ImageDetailComponent } from './modules/gallery/image-detail/image-detail.component';
-import { UploadComponent } from './modules/gallery/upload/upload.component';
-import { GalleryComponent } from './modules/gallery/gallery/gallery.component';
-import { EmailLoginComponent } from './modules/user-administration/login/email-login/email-login.component';
-import { AuthGuard } from './services/guards/auth-guard.service';
-import { SignupComponent } from './modules/user-administration/signup/signup.component';
-import { LoginComponent } from './modules/user-administration/login/login.component';
 import { NotFoundComponent } from './modules/shared/not-found/not-found.component';
-import { HomeComponent } from './modules/home/home/home.component';
-import { NavbarComponent } from './modules/shared/navbar/navbar.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -19,21 +9,12 @@ const routes: Routes = [
     loadChildren: './modules/home/home.module#HomeModule'
   },
   {
-    path: '',
+    path: 'user',
     loadChildren: './modules/user-administration/user-administration.module#UserAdministrationModule'
   },
   {
     path: 'gallery',
-    component: GalleryComponent
-  },
-  {
-    path: 'image/:id',
-    component: ImageDetailComponent
-  },
-  {
-    path: 'upload',
-    component: UploadComponent,
-    canActivate: [AuthGuard]
+    loadChildren: './modules/gallery/gallery.module#GalleryModule'
   },
   {
     path: '**',
