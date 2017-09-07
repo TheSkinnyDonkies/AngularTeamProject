dc9b80a4dbfb7648e24501ec07c470af6c8237e5
shopov91 <shopov91@gmail.com>
2017-09-07 13:39:40 +0300

    home component is now lazy loaded

--- TeamApp/src/app/app-routing.module.ts
+++ TeamApp/src/app/app-routing.module.ts
@@ -16,12 +16,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    component: HomeComponent
+    loadChildren: './modules/home/home.module#HomeModule'
   },
   {
     path: 'login',
